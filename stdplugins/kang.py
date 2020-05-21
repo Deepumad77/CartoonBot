45a46b93216b3fbe2963abd8868fe0a85f56c85c
Deepumad77 <63442640+Deepumad77@users.noreply.github.com>
2020-05-21 09:42:27 +0530

    Update kang.py

--- stdplugins/kang.py
+++ stdplugins/kang.py
@@ -67,7 +67,7 @@ async def _(event):
     name = me.username
     userid = event.from_id
     packname = f"Babyツ @Baby_xD"
-    packshortname = f"Darling_Baby-xD{userid}"  # format: Uni_Borg_userid
+    packshortname = f"Darling_BabyxD{userid}"  # format: Uni_Borg_userid
 
     is_a_s = is_it_animated_sticker(reply_message)
     file_ext_ns_ion = "chutiya_Sticker.png"
